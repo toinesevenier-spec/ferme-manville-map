211915a5883ff18bb9da35500477b5b91bfa906a
toinesevenier-spec <toine.sevenier@gmail.com>
2025-11-25 12:07:16 +0100

    Update vite.config.ts

--- public/vite.config.ts
+++ public/vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/nom-du-repo/", // remplace par le nom exact de ton dépôt
+  base: "/ferme-manville-map/", // remplace par le nom exact de ton dépôt
 });
